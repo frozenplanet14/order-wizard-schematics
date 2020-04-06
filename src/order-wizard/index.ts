69f93e2ea57ef8fb38bece2ac0e4198add7d3968
Suman Jha <suman.jha@fiserv.com>
2020-04-05 20:35:33 -0700

    adding import to existing file and running npm taks such as package install

--- src/order-wizard/index.ts
+++ src/order-wizard/index.ts
@@ -1,6 +1,8 @@
 import { Rule, SchematicContext, Tree, url, apply, move, mergeWith, MergeStrategy, template, filter, SchematicsException, chain } from '@angular-devkit/schematics';
 import { normalize, strings, experimental } from '@angular-devkit/core';
-
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+import { NodePackageTaskOptions } from '@angular-devkit/schematics/tasks/node-package/options';
+import * as ts from 'typescript';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
@@ -22,7 +24,8 @@ export function orderWizard(_options: any): Rule {
 
     const templateRule = mergeWith(newTree, MergeStrategy.Default);
     const updateModuleRule = updateRootModule(_options, workspace);
-    const chainedRule = chain([templateRule, updateModuleRule]);
+    const installMaterialRule = installMaterial();
+    const chainedRule = chain([templateRule, updateModuleRule, installMaterialRule]);
     return chainedRule(tree, _context);
   };
 }
@@ -59,10 +62,98 @@ function updateRootModule(_options: any, workspace: any): Rule {
     const rootModulePath = `${project.root}/${project.sourceRoot}/${project.prefix}/${project.prefix}.module.ts`;
     const importContent = `import { ${exportedModuleName}Module } from './${modulePath}/${moduleName}/${moduleName}.module';\n`;
 
+    const moduleFile = getAsSourceFile(tree, rootModulePath);
+    const lastImportEndPos = findLastImportEndPos(moduleFile);
+    const importArrayEndPos = findImportArray(moduleFile);
+
     const rec = tree.beginUpdate(rootModulePath);
-    rec.insertLeft(0, importContent);
+    rec.insertLeft(lastImportEndPos + 1, importContent);
+    rec.insertLeft(importArrayEndPos - 1, `, ${exportedModuleName}Module`);
     tree.commitUpdate(rec);
 
     return tree
+  }
+}
+
+function getAsSourceFile(tree: Tree, path: string): ts.SourceFile {
+  const file = tree.read(path);
+  if (!file) {
+    throw new SchematicsException(`${path} not found`);
+  }
+  return ts.createSourceFile(
+    path,
+    file.toString(),
+    ts.ScriptTarget.Latest,
+    true
+  );
+}
+
+function findLastImportEndPos(file: ts.SourceFile): number {
+  let pos: number = 0;
+  file.forEachChild((child: ts.Node) => {
+    if (child.kind === ts.SyntaxKind.ImportDeclaration) {
+      pos = child.end;
+    }
+  });
+  return pos;
+}
+
+function findImportArray(file: ts.SourceFile): number {
+  let pos: number = 0;
+
+  file.forEachChild((node: ts.Node) => {
+    if (node.kind === ts.SyntaxKind.ClassDeclaration) {
+      node.forEachChild((classChild: ts.Node) => {
+        if (classChild.kind === ts.SyntaxKind.Decorator) {
+          classChild.forEachChild((moduleDeclaration: ts.Node) => {
+            moduleDeclaration.forEachChild((objectLiteral: ts.Node) => {
+              objectLiteral.forEachChild((property: ts.Node) => {
+                if (property.getFullText().includes('imports')) {
+                  pos = property.end;
+                }
+              });
+            });
+          });
+        }
+      });
+    }
+  });
+
+  return pos;
+}
+
+function installMaterial(): Rule {
+  return (tree: Tree, _context: SchematicContext): Tree => {
+    const packageJsonPath = '/package.json';
+    const materialDepName = '@angular/material';
+    const packageJson = getAsSourceFile(tree, packageJsonPath);
+    let materialInstalled = false;
+
+    packageJson.forEachChild((node: ts.Node) => {
+      if (node.kind === ts.SyntaxKind.ExpressionStatement) {
+        node.forEachChild(objectLiteral => {
+          objectLiteral.forEachChild(property => {
+            if (property.getFullText().includes('dependencies')) {
+              property.forEachChild(dependency => {
+                if (dependency.getFullText().includes(materialDepName)) {
+                  _context.logger.info('Angular Material already installed');
+                  materialInstalled = true;
+                }
+              });
+            }
+          });
+        });
+      }
+    });
+
+    if (!materialInstalled) {
+      const options = <NodePackageTaskOptions>{
+        packageName: materialDepName
+      };
+      _context.addTask(new NodePackageInstallTask(options));
+      _context.logger.info('Installing Angular Material');
+    }
+
+    return tree;
   }
 }
